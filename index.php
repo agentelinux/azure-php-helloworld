b2c36ce3e9dbc0baa94ecf442616dcfb79de4979
Sarah Withee <github@geekygirlsarah.com>
2017-07-24 18:13:35 -0500

    Updated with better 'hello world'

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<html>
+<head>
+<title>A test app!</title>
+</head>
+<body>
 
-echo "Hello World!";
+<?php
+if (isset($_POST["name"]) && $_POST["name"] <> "") {
+    echo "Hello there, " . $_POST["name"];
+}
+else {
+?>
+
+Hi there! What's your name?
+
+
+<form action="./" method="post">
+    <input type="text" name="name" />
+    <input type="submit" />
+</form>
+
+<?php 
+}
+
 
 ?>
